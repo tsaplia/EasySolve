142c011932769f57e3616c3821bced5b4ddfee40
TeaDuke <maxchernx@gmail.com>
2023-12-19 15:31:01 +0200

    Start do design for app, add new font

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { FormsModule } from '@angular/forms';
+// import { AlertModule } from 'ngx-bootstrap'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    // AlertModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
