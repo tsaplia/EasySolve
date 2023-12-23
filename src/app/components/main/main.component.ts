a2672cdca1e75cc2bd2537d40206276735d35fd1
TeaDuke <maxchernx@gmail.com>
2023-12-23 15:49:28 +0200

    Mathjax work, array of formulas

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,6 +1,10 @@
-import { Component, ElementRef, OnInit } from "@angular/core";
+import { ChangeDetectorRef, Component, ElementRef, OnInit, Renderer2, ViewChild } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
 import { FormulaModelComponent } from "../formula-model/formula-model.component";
+import { timeInterval } from "rxjs";
+
+declare let MathJax: any;
+
 
 @Component({
   selector: 'main-component',
@@ -8,24 +12,29 @@ import { FormulaModelComponent } from "../formula-model/formula-model.component"
   styleUrls: ['main.component.scss']
 })
 export class MainComponent implements OnInit {
-  constructor(private dialog: MatDialog) {}
+  @ViewChild("render") qqq: ElementRef;
   
   text: string = "";
+  lines: string[] = [];
   
+  constructor(private dialog: MatDialog, private renderer: Renderer2, private cdRef: ChangeDetectorRef) {}
+
   ngOnInit(): void {
-    
+    //MathJax.typeset([document.getElementById("render")]);
   }
   
-  OpenAddFunction() {
+  openAddFunction() {
     var formulaDialog = this.dialog.open(FormulaModelComponent);
     formulaDialog.afterClosed().subscribe(resp => {
-      console.log(resp);
-      this.text = resp.formula;
-      
+      if(!resp) return;
+      this.lines.push(resp.formula);
+      this.cdRef.detectChanges();
+      MathJax.typeset([document.getElementById((this.lines.length-1).toString())]);
     });
   }
-  
 
-  
- 
+  clear() {
+    this.lines = [];
+    this.cdRef.detectChanges();
+  }
 }
